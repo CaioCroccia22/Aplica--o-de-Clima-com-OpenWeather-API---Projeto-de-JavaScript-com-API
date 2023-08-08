185a4ec636de86f9810a3de165b22dae5e5cf0be
caiocroccia22 <caiocroccia22@gmail.com>
2023-08-08 18:05:36 -0300

    Evento botão de pesquisa

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// 7b7c3605fe8753e1dd6d061a46dc4ecd
+const apiKey = "7b7c3605fe8753e1dd6d061a46dc4ecd";
+const apiCountryUrl = "https://www.countryflagicons.com/SHINY/64/png"
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+
+//Funções
+
+
+//Eventos
+//Clique do botão
+searchBtn.addEventListener("click", (e) => {
+
+
+    e.preventDefault();
+    console.log("teste");
+})
