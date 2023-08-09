5576214aaf7be141261cb1d2812d05b768441d90
caiocroccia22 <caiocroccia22@gmail.com>
2023-08-09 20:00:51 -0300

    Adição de outras constantes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7b7c3605fe8753e1dd6d061a46dc4ecd
 const apiKey = "7b7c3605fe8753e1dd6d061a46dc4ecd";
 const apiCountryUrl = "https://www.countryflagicons.com/SHINY/64/png"
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city-input");
+const tempElement = document.querySelector("#temperature-span")
+
 
 //Funções
+const showWeatherData = (city) => {
+    console.log(city);
+}
 
 
 //Eventos
-//Clique do botão
+//Clique do botão da cidade
 searchBtn.addEventListener("click", (e) => {
 
 
     e.preventDefault();
-    console.log("teste");
+
+
+    //Pegar o valor do input
+    const city = cityInput.value;
+
+    showWeatherData(city);
 })
