c9cfcc40643fa7ed1941e7c79f1b836c54e490b2
caiocroccia22 <caiocroccia22@gmail.com>
2023-08-11 19:05:50 -0300

    Função para trocar o nome da localidade

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=7b7c3605fe8753e1dd6d061a46dc4ecd
+
 const apiKey = "7b7c3605fe8753e1dd6d061a46dc4ecd";
+//7b7c3605fe8753e1dd6d061a46dc4ecd
+
 const apiCountryUrl = "https://www.countryflagicons.com/SHINY/64/png"
 
 const cityInput = document.querySelector("#city-input");
@@ -14,8 +16,26 @@ const umidityElement = document.querySelector('#umidity span');
 const windElement = document.querySelector('#wind span');
 
 //Funções
-const showWeatherData = (city) => {
-    console.log(city);
+//Consulta dos dados da API
+//Função assincrona pois é uma API e pode demorar um tempo para responder
+const getWeatherData = async (city) => {
+
+    
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    //Fetch - recursos do js para realizar requisições assincronas
+    const res = await fetch(apiWeatherURL);
+    //Dados chegam em JSON e é transformado em objeto JS
+    const data = await res.json();
+
+    return data;
+
+
+}
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
 }
 
 
